8880550c0e094c85a56e9c5a31fab8492afdb2a7
Shanayna De Ugarte <shany.de.ugarte.q@gmail.com>
2026-09-09 18:47:40 -0400

    test(totalizador): agregar prueba fallida para validacion de cantidad y precio

--- src/totalizador.test.js
+++ src/totalizador.test.js
@@ -4,4 +4,10 @@ describe("Totalizador de Ventas - Slice 1: Precio Neto", () => {
   it("deberia calcular el precio neto multiplicando cantidad por precio unitario", () => {
     expect(calcularPrecioNeto(20, 3)).toEqual(60);
   });
+
+  it("deberia retornar un mensaje de error si la cantidad o el precio son menores o iguales a cero", () => {
+    expect(calcularPrecioNeto(-5, 10)).toEqual("Cantidad invalida");
+    expect(calcularPrecioNeto(5, 0)).toEqual("Precio invalido");
+  });
 });
+
