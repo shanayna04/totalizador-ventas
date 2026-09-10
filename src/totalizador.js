8e03a5b31c4dc801d37042b3bb48519d75c8107a
Shanayna De Ugarte <shany.de.ugarte.q@gmail.com>
2026-09-09 21:20:30 -0400

    feat(totalizador): implementar tramos completos de descuento por volumen

--- src/totalizador.js
+++ src/totalizador.js
@@ -24,9 +24,11 @@ function obtenerImpuestoEstado(estado) {
 }
 
 function obtenerPorcentajeDescuento(precioNeto) {
-  if (precioNeto >= 1000) {
-    return 0.03;
-  }
+  if (precioNeto >= 30000) return 0.15;
+  if (precioNeto >= 10000) return 0.1;
+  if (precioNeto >= 7000) return 0.07;
+  if (precioNeto >= 3000) return 0.05;
+  if (precioNeto >= 1000) return 0.03;
   return 0;
 }
 
